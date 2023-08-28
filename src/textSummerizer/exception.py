afab1555d76201801ada04017c745bac560ec829
meghdad <mehdizadeh.iust@gmail.com>
2023-08-28 10:12:55 -0400

    requiremnets, logger and exception is done!

---
+++ src/textSummerizer/exception.py
@@ -0,0 +1,15 @@
+import sys
+
+def error_message_detail(error, error_details:sys):
+    _ ,_ ,ext_tb = error_details.exc_info()
+    file_name = ext_tb.tb_frame.f_code.co_filename
+    line_number = ext_tb.tb_lineno
+    error_message = f"Error occured in python script name [{file_name}] line number [{line_number}] error message [{str(error)}]"
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_details=error_details)
+    def __str__(self):
+        return self.error_message
